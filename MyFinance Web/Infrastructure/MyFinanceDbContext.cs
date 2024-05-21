9a6296537b9e105eaa54f41c767270880d33ebea
Bruno Honda <bruno.honda@live.com>
2024-05-21 11:10:02 -0300

    implements database connection

---
+++ MyFinance Web/Infrastructure/MyFinanceDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFinance_Web.Domain;
+
+namespace MyFinance_Web.Infrastructure
+{
+    public class MyFinanceDbContext : DbContext
+    {
+        public DbSet<PlanoConta> PlanoConta { get; set; }
+        public DbSet<Transacao> Transacao { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=DESKTOP-E4K97VN;Database=myfinance;Trusted_Connection=True;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
